be15fec36cc043d14599ed2da7768a85ae692183
mshr1-creat <19950786m@gmail.com>
2024-10-23 14:36:51 +0900

    サーバーの実装
    Express を使用

---
+++ server/index.js
@@ -0,0 +1,12 @@
+// server/index.js
+import express from 'express';
+const app = express();
+const port = 9000;
+
+app.get('/', (req, res) => {
+  res.send('Hello world');
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
